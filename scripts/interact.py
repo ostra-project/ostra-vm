8dd22283ef34ff7ca555aca6d36c93cad751902c
Bibollet Adrien <71235233+yoratoni@users.noreply.github.com>
2021-12-09 10:16:08 +0100

    Cleared Interact.py

--- scripts/interact.py
+++ scripts/interact.py
@@ -5,35 +5,11 @@ from brownie import Ostra
 def setGwei(amount):
     return amount * 10**8
 
-
-def burn(contract, txFrom, txBurn):
-    contract.burn(txBurn, txFrom)
-    printBurn = contract.totalBurn()
-    printTotalSupply = contract.totalSupply()
-
-    pyprint(txBurn, 'Expected Burn')
-    pyprint(printBurn, 'Burn')
-    pyprint(printTotalSupply, 'Total Supply')
-
-
-def transfer(contract, account, address, amount):
-    txFrom = {'from': account}
-    contract.transfer(address, amount, txFrom)
-    printBalanceFrom = contract.balanceOf(account.address)
-    printBalanceTo = contract.balanceOf(address)
-
-    pyprint(printBalanceFrom, 'Sender Balance')
-    pyprint(printBalanceTo, 'Receiver Balance')
-
-
 def interact():
     account = getAccount()
     txFrom = {'from': account}
     contract = Ostra[-1]  # Get Last Contract
     
-    for _ in range(0, 10):
-        transfer(contract, account, '0xcE4DDc88807abEe52E6d477d41fcd955AFc3A9a4', setGwei(10)) 
-
 
 def main():
     interact()
