199632be0fe63251789f89078f3bc7a7ad38f9c2
Bibollet Adrien <71235233+yoratoni@users.noreply.github.com>
2021-12-09 21:37:29 +0100

    en-US pyprint() number format

--- scripts/lib.py
+++ scripts/lib.py
@@ -3,10 +3,18 @@ from brownie import (
     network,
     config
 )
+import brownie
 
 
-MAIN_ENVIRONMENTS = ["bsc-fork"]
-PANCAKESWAP_ROUTER = '0x05fF2B0DB69458A0750badebc4f9e13aDd608C7F'
+MAIN_ENVIRONMENTS = ["bsc-fork", "bsc-main"]
+
+if network.show_active() in MAIN_ENVIRONMENTS:
+    PANCAKESWAP_ROUTER = '0x05fF2B0DB69458A0750badebc4f9e13aDd608C7F'
+else:
+    PANCAKESWAP_ROUTER = '0xD99D1c33F9fC3444f8101754aBC46c52416550D1'
+
+
+DECIMALS = 10**8
 
 
 init_pyprint = False
@@ -20,11 +28,19 @@ def pyprintInit():
 
 def pyprint(msg = 'Null', title = None):
     pyprintInit()
-    if title != None:
-        print('==>', title + ':', msg)
-    else:
-        print('==>', msg)
 
+    output = str(msg)
+    outputInt = 0
+
+    if output.isdecimal():
+        outputInt = int(output)
+        outputInt = outputInt / DECIMALS
+        output = f'{outputInt:,}'
+
+    if title != None:
+        print('==>', title + ':', output)
+    else:
+        print('==>', output)
 
 
 def getAccount(index = None, ID = None):
