e05be6a752d88b6ef11e36421d5c5bdc1918090b
Bibollet Adrien <71235233+yoratoni@users.noreply.github.com>
2021-12-22 10:44:35 +0100

    Starting Diamond Setup

---
+++ contracts/OSTRA.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.10;
+
+
+contract OSTRA {
+    constructor(address _owner, address _diamondCutFacet) payable {
+
+    }
+
+
+    receive() external payable {}
+}
