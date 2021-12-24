1e8dde789f7d599aaf4190ead69c46d948cdeb43
Bibollet Adrien <71235233+yoratoni@users.noreply.github.com>
2021-12-24 10:36:32 +0100

    New test wallets

--- scripts/settings.py
+++ scripts/settings.py
@@ -8,7 +8,7 @@ from brownie import network
 # contractName -> 'contract_size.py' script checks the ./build/contracts/contractName.json file deployedBytecode key
 # contractSizeLimit -> Default Ethereum Smart Contract size limit is 24KB
 # contractDecimals -> Decimals (Min amount of the token) allows pyprint to automatically divide numbers
-contractName = 'Main'
+contractName = 'Ostra'
 contractFolder = '../build/contracts/'
 contractSizeLimit = 24 * 1024
 contractDecimals = 10**8
@@ -28,10 +28,10 @@ userAccount = ['real-account']  # Brownie encrypted account names
 # Test Wallets
 # Generated from https://vanity-eth.tk/
 testWallets = [
-    '0x0A5C946949b4cc1a80E332Aa89ACC5aa9017E206',
-    '0x2c211b0EdC9449673385209651845bE6d7d003f2',
-    '0x0dcBd09De3335181EeB5DC8A0aF29807186aB87a',
-    '0x5A9310629F9f9320b7E7E66c2de40367a0B3856d'
+    '0xD0583e2195E866d9E773C4Ef6Ba21BdDa3732b7B',
+    '0x7eC0BfD42eA25f3826668C0574249D8e87cceB38',
+    '0x509f3d0D9abCDc94F3189E35322F7D655e0872e1',
+    '0xB5318747A65398f8EEcB303805f3176D7e0F5291'
 ]
 
 
